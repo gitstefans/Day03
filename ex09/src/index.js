51b6d38bde0d099a3b84299342dd1413686a7077
Stefan Skočo <stefansgit@outlook.com>
2021-03-10 16:48:57 +0100

    difference

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,6 +1,6 @@
 function main(numOne, numTwo) {
   // Only change code below this line
-  difference = numOne - numTwo;
+  var difference = numOne - numTwo;
   //Only change code above this line
   return difference;
 }
