c25e2932c47aec74c5e78849802f75f9b2cf1108
Stefan Skočo <stefansgit@outlook.com>
2021-03-10 15:29:04 +0100

    sum of 2 variables

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -5,8 +5,8 @@ function main(numOne, numTwo) {
   return sum;
 }
 
-console.log(main(15,8));
-console.log(main(5,28));
-console.log(main(150,425));
+console.log(main(15,8));  //change this line
+console.log(main(5,28));  //change this line
+console.log(main(150,425)); //change this line
 
 module.exports = main;
